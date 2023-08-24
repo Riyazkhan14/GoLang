8f974982b2416e7225cc6219eb4a0361add10c46
Riyazkhan14 <21050074+Riyazkhan14@users.noreply.github.com>
2023-08-24 22:55:41 +0530

    Writing new codes

--- 02Variables/main.go
+++ 02Variables/main.go
@@ -18,4 +18,23 @@ func main() {
 	var isFloat float32 = 2000.222020292
 	fmt.Println(isFloat)
 	fmt.Printf("Type of Variable is : %T \n\n", isFloat)
+
+	var smallFloat float64 = 255.455245584584564444565
+	fmt.Println(smallFloat)
+	fmt.Printf("Type of Variable is : %T \n\n", smallFloat)
+
+	// implicit type
+
+	var website = "www.google.com"
+	fmt.Println(website)
+
+	// no var style
+
+	var numberOfUser := 35220.0
+	fmt.Println(numberOfUser)
+
+	var LoginToken = "Hello World"
+	fmt.Println(LoginToken)
+	fmt.Printf("Varibale is of type : %T \n", LoginToken)
+
 }
